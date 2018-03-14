11055cb195e003d8003fbd14e6407d4891ef3bc3
dev <dev@macbook-air-de-bruno.home>
2018-03-14 11:24:52 +0100

    wip

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+'use strict';
+const {google} = require('googleapis');
+const analytics = google.analytics('v3');
+
+var keys = require('/Users/dev/vscode-ts-node-debugging/LoGADashboard-0ca2ec0a14cb.json');
+var viewID = 'ga:160890197';
 var express = require('express');
 var bodyParser = require('body-parser');
+
+
 var _ = require('lodash');
 var app = express();
 
@@ -91,8 +99,7 @@ app.all('/annotations', function(req, res) {
   res.json(annotations);
   res.end();
 })
-
-app.all('/query', function(req, res){
+app.all('/query1', function(req, res){
   setCORSHeaders(res);
   console.log(req.url);
   console.log(req.body);
@@ -115,7 +122,45 @@ app.all('/query', function(req, res){
   res.json(tsResult);
   res.end();
 });
+app.all('/query', function(req, res){
+  setCORSHeaders(res);
+  console.log(req.url);
+  console.log(req.body);
+  
+  var seriesList = [];
 
+  var jwtClient = new google.auth.JWT(keys.client_email, null , keys.private_key, ['https://www.googleapis.com/auth/analytics.readonly'], null);
+  jwtClient.authorize(function (err, tokens) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  
+    analytics.data.ga.get({
+      'auth': jwtClient,
+      'ids': viewID,
+      'metrics': 'ga:pageviews',
+      'start-date': 'yesterday',
+      'end-date': 'today',
+    }, function (err, response) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      //console.log(JSON.stringify(response.data.rows);
+      var dp = []
+      _.each(response.data.rows, function(kk) {
+        dp.push([parseInt(kk), 0])
+      });
+      seriesList.push({"target": "ga:pageviews", "datapoints": dp});
+      res.json(seriesList);
+      res.end();
+    });
+  });
+
+
+
+});
 app.listen(3333);
 
 console.log("Server is listening to port 3333");
